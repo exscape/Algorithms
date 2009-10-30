3fd6c11714cf386043841b487b0fd4a42245aa8d
Thomas <serenity@exscape.org>
2009-10-30 18:22:24 +0100

    Add Length() and Pop() plus some tests

--- linked_list.c
+++ linked_list.c
@@ -71,6 +71,14 @@ int Count(node **head, const char *needle) {
 	return count;
 }
 
+size_t Length(node **head) {
+	size_t len = 0;
+	for (node *n = *head; n != NULL; n = n->next)
+		len++;
+
+	return len;
+}
+
 /* LinkedListProblems.pdf problem #2 */
 char *GetNth(node **head, int index) {
 	int i=0;
@@ -95,14 +103,31 @@ void free_list(node **head) {
 	*head = NULL;
 }
 
+/* LinkedListProblems.pdf problem #4 */
+char *Pop(node **head) {
+	if (Length(head) == 0)
+		return NULL;
+	
+	node *new_head = (*head)->next;
+	char *data = strdup((*head)->data);
+	free(*head);
+	*head = new_head; /* No special case needed for NULL */
+
+	return data;
+}
+
 int main() {
 	node *list = calloc(1, sizeof(node));
+	printf("Length after allocating: %zu\n", Length(&list));
 	set_node_data(&list, "Alpha");
+	printf("Length after adding 1 element: %zu\n", Length(&list));
 
 	create_add_node(&list, "Beta", START);
 	create_add_node(&list, "Gamma", END);
 	create_add_node(&list, "Omega", START);
 	create_add_node(&list, "Alpha", END);
+
+	printf("Length with 5 elements: %zu\n", Length(&list));
 
 	print_list(&list);
 
@@ -114,7 +139,16 @@ int main() {
 //	printf("GetNth(0): %s\n", GetNth(&list, 0));
 //	printf("GetNth(2): %s\n", GetNth(&list, 2));
 //	printf("GetNth(20): %s\n", GetNth(&list, 20));
+//
+
+	for (int i=0; i<3; i++) {
+		char *s = Pop(&list);
+		printf("Pop: %s\n", s);
+		printf("New length: %zu\n", Length(&list));
+		free(s);
+	}
 
 	free_list(&list);
+	printf("Length after freeing: %zu\n", Length(&list));
 	return 0;
 }
